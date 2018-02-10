19d4e10aeac109c9c939b887859a84b36ed02d1d
Mhage <masahage@soleil.ocn.ne.jp>
2018-02-10 11:32:29 +0900

    Add files via upload

---
+++ I2S.cpp
@@ -0,0 +1,29 @@
+#include "I2S.h"
+
+void I2S_Init() {
+  i2s_config_t i2s_config = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+    .sample_rate = SAMPLE_RATE,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+    .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,                           //2-channels
+    .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+    .intr_alloc_flags = 0,
+    .dma_buf_count = 16,
+    .dma_buf_len = 60
+  };
+
+  i2s_pin_config_t pin_config = {
+    .bck_io_num = PIN_I2S_BCLK,
+    .ws_io_num = PIN_I2S_LSR,
+    .data_out_num = PIN_I2S_DOUT,
+    .data_in_num = -1   //Not used
+  };
+  i2s_driver_install((i2s_port_t)0, &i2s_config, 0, NULL);
+  i2s_set_pin((i2s_port_t)0, &pin_config);
+  i2s_set_clk((i2s_port_t)0, SAMPLE_RATE, (i2s_bits_per_sample_t)16, (i2s_channel_t)2);
+}
+
+void I2S_Write(char* data, int numData) {
+    i2s_write_bytes((i2s_port_t)0, (const char *)data, numData, portMAX_DELAY);
+}
+
---
+++ I2S.h
@@ -0,0 +1,12 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/i2s.h"
+#include "esp_system.h"
+#define SAMPLE_RATE (44100)
+#define PIN_I2S_BCLK 26
+#define PIN_I2S_LSR 22
+#define PIN_I2S_DOUT 25
+
+// 44100Hz, 16bit, stereo
+void I2S_Init();
+void I2S_Write(char* data, int numData);
