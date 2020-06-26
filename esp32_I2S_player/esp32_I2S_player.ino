1cefacd2df322b6ef704b20724da9f9afa84cd0f
Mhage <masahage@soleil.ocn.ne.jp>
2020-06-26 22:36:55 +0900

    Update esp32_I2S_player.ino

--- esp32_I2S_player/esp32_I2S_player.ino
+++ esp32_I2S_player/esp32_I2S_player.ino
@@ -4,16 +4,29 @@
 
 const int offset = 0x2C;
 char data[800];
+char stereoData[1600];
 
 void setup() {
   SD.begin();
-  File file = SD.open("/sound.wav");  // 44100Hz, 16bit, stereo, linear PCM
+  File file = SD.open("/sound_s.wav");  // 44100Hz, 16bit, linear PCM
+  file.seek(22);
+  int ch = file.read();
   file.seek(offset);
   I2S_Init();
   while (file.readBytes(data, sizeof(data))) {
-    I2S_Write(data, sizeof(data));
+    if (ch == 2) I2S_Write(data, sizeof(data));
+    else if (ch == 1) {
+      for (int i = 0; i < sizeof(data); ++i) 
+      {
+        stereoData[4 * (i / 2) + i % 2] = data[i];
+        stereoData[4 * (i / 2) + i % 2 + 2] = data[i];
+      }
+      I2S_Write(stereoData, sizeof(stereoData));
+    }
   }
   file.close();
+  for (int i = 0; i < sizeof(data); ++i) data[i] = 0; // to prevent buzzing
+  for (int i = 0; i < 5; ++i) I2S_Write(data, sizeof(data));
 }
 
 void loop() {
